014cf7358d89711eda93dd019e174045f968df31
Dirk Weil <dirk.weil@gedoplan.de>
2019-11-25 18:36:26 +0100

    Übung 5

--- skipass-gate/src/main/java/jvmcon/skipass/gate/service/GateService.java
+++ skipass-gate/src/main/java/jvmcon/skipass/gate/service/GateService.java
@@ -1,11 +1,14 @@
 package jvmcon.skipass.gate.service;
 
 import java.time.LocalDateTime;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 
 import org.eclipse.microprofile.faulttolerance.Fallback;
+import org.eclipse.microprofile.reactive.messaging.Incoming;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 
 import jvmcon.skipass.gate.api.SkiPassManager;
@@ -13,14 +16,16 @@ import jvmcon.skipass.gate.api.SkiPassManager;
 @ApplicationScoped
 public class GateService {
 
+  ConcurrentMap<String, LocalDateTime> cache = new ConcurrentHashMap<>();
+
   @Inject
   @RestClient
   SkiPassManager skiPassManager;
 
   @Fallback(fallbackMethod = "returnTrue")
   public boolean isValid(String skiPassId) {
-    LocalDateTime validTo = fetch(skiPassId);
-    // LocalDateTime validTo = this.cache.computeIfAbsent(skiPassId, this::fetch);
+    // LocalDateTime validTo = fetch(skiPassId);
+    LocalDateTime validTo = this.cache.computeIfAbsent(skiPassId, this::fetch);
     return validTo != null && validTo.isAfter(LocalDateTime.now());
   }
 
@@ -32,5 +37,10 @@ public class GateService {
   private boolean returnTrue(String skiPassId) {
     System.out.println("Using default value!!!!");
     return true;
+  }
+
+  @Incoming("skipassInvalidation")
+  public void invalidate(String skiPassId) {
+    this.cache.remove(skiPassId);
   }
 }
--- skipass-manager/src/main/java/jvmcon/skipass/manager/rest/SkiPassResource.java
+++ skipass-manager/src/main/java/jvmcon/skipass/manager/rest/SkiPassResource.java
@@ -4,15 +4,21 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import javax.inject.Inject;
+import javax.transaction.Transactional;
 import javax.ws.rs.BadRequestException;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.NotFoundException;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
+import io.smallrye.reactive.messaging.annotations.Channel;
+import io.smallrye.reactive.messaging.annotations.Emitter;
 import jvmcon.skipass.manager.entity.SkiPass;
 import jvmcon.skipass.manager.persistence.SkiPassRepository;
 
@@ -52,6 +58,12 @@ public class SkiPassResource {
   @Path("{id}/validTo")
   @Produces(MediaType.APPLICATION_JSON)
   public LocalDateTime getValidTo(@PathParam("id") String id) {
+    try {
+      Thread.sleep(3000);
+    } catch (InterruptedException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
     SkiPass skiPass = this.skiPassRepository.findById(id);
     LocalDateTime now = LocalDateTime.now();
     return skiPass != null && skiPass.getValidFrom().isBefore(now) && skiPass.getValidTo().isAfter(now) && !skiPass.isBlocked()
@@ -59,4 +71,24 @@ public class SkiPassResource {
         : LocalDateTime.MIN;
 
   }
+
+  @Inject
+  @Channel("skipassInvalidation")
+  Emitter<String> skipassInvalidationEmitter;
+
+  @Path("{id}/blocked")
+  @PUT
+  @Consumes(MediaType.APPLICATION_JSON)
+  @Transactional(rollbackOn = Exception.class)
+  public void setBlocked(@PathParam("id") String id, boolean blocked) {
+    SkiPass skiPass = this.skiPassRepository.findById(id);
+    if (skiPass == null) {
+      throw new NotFoundException();
+    }
+
+    skiPass.setBlocked(blocked);
+
+    this.skipassInvalidationEmitter.send(id);
+  }
+
 }
